77f650881b140f6b2da7bb11451c9e049a7b42b8
Braden-Phetsarath <bkphetsarath@students.unwsp.edu>
2025-09-18 09:27:39 -0500

    Update program_2.py

--- program_2.py
+++ program_2.py
@@ -1,3 +1,14 @@
+#get input : Person's age
+#display age
+#if age is in range 1 or less
+#then display infant
+#if age is in range greater than 1 , but less than 13
+#then display child
+#if age is in range 13 or greater but less than 20
+#then display teenager
+#if age is in range greater than 20
+#then display adult
+
 # Write a program that asks the user to enter a person's age.  The program should display a message indicating whether the person is an infant, a child, a teenager, or an adult.  Following are the guidelines:
 
 # If the person is 1 year old or less, it should display "infant" (without quotes).
@@ -9,6 +20,10 @@ def categorize_age(age):
     ageCategory = "TBD"
     ######################
     # WRITE YOUR CODE HERE
+    if age <= 1: ageCategory = "infant"
+    if age >= 1 and age < 13: ageCategory = "child"
+    if age >= 13 and age <20: ageCategory = "Teenager"
+    if age >= 20: ageCategory ="adult"
     ######################
 
 
